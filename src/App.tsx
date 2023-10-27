7da8ae7b85cb3431fa906ee4818c0324fb19c1fc
kiya kebe <kiyakebe799@gmail.com>
2023-10-27 10:51:10 +0300

    created: fetch requst

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import axios, { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+interface User {
+  id: number;
+  name: string;
+  profile: ImageData;
+}
+
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setIsLoading(true);
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setIsLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  const deleteUser = (user: User) => {
+
+  }
+
+  return (
+    <>
+      {isLoading && <div className="spinner-border"></div>}
+      {error && <p className="text-danger">{error}</p>}
+      <ul className="list-group w-50">
+        {users.map((user) => (
+          <li key={user.id} className="list-group-item d-flex justify-content-between">
+            {user.name}{" "}
+            <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
